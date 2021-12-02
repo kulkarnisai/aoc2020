46f0b3151b759eedae73d03356ae3a7e077443ce
Sai Kulkarni <saikulkarni0@gmail.com>
2021-12-02 15:55:14 -0500

    updated template

--- dn/solution.py
+++ dn/solution.py
@@ -1,16 +1,23 @@
-def FNAME1(fname):    
+def FNAME1(arr):    
+   return 0
+
+
+def FNAME2(arr):    
+   return 0
+
+def preprocess(fname):
     with open(fname) as f:
-        inarr = f.readlines()
+        arr = f.readlines()
 
-
-def FNAME2(fname):    
-    with open(fname) as f:
-        inarr = f.readlines()
-
+    return arr
 
 if __name__ == "__main__":
-    assert FNAME1("test_input") == ANS1
-    print("P1:\t" + str(FNAME1("input")))
 
-    assert FNAME2("test_input", 3) == ANS2
-    print("P2:\t" + str(FNAME2("input", 3)))
+    test_arr = preprocess("test_input")
+    arr = preprocess("input")
+    
+    assert FNAME1(test_arr) == 0
+    print("P1:\t" + str(FNAME1(arr)))
+
+    assert FNAME2(test_arr) == 0
+    print("P2:\t" + str(FNAME2(arr)))
