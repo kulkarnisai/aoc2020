0489d21710e32482159a18b0eab43392217f9cbe
Sai Kulkarni <saikulkarni0@gmail.com>
2021-12-02 21:23:01 -0500

    updating number scheme

---
+++ d03/test_solution.py
@@ -0,0 +1,41 @@
+
+import os
+import sys
+def FNAME1(arr):    
+   return 0
+
+
+def FNAME2(arr):    
+   return 0
+
+def preprocess(fname):
+    fpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), fname)
+    with open(fpath) as f:
+        arr = f.readlines()
+
+    return arr
+
+def test_day3(outfile):
+
+    test_arr = preprocess("test_input")
+    arr = preprocess("input")
+
+    if outfile:
+        log = open(outfile, 'a')
+        sys.stdout = log  
+        header = '## '
+        codeblock = '\n```'
+    else:
+        header = ''
+        codeblock = ''
+
+    print(header + "Day 3 Results:" + codeblock)
+
+    assert FNAME1(test_arr) == 0
+    print("P1:\t" + str(FNAME1(arr)))
+
+    assert FNAME2(test_arr) == 0
+    print("P2:\t" + str(FNAME2(arr)) + codeblock)
+
+if __name__ == "__main__":
+    test_day3(None)
